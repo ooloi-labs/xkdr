e6784a1806bf934154f8aa25083fc6354485c9cc
Sandeep Kadam <Sandeepkadam3007@gmail.com>
2023-06-16 12:37:04 +0530

    Styled Table data

--- src/pages/Form/comps/SurveyTable/styled.index.js
+++ src/pages/Form/comps/SurveyTable/styled.index.js
@@ -3,30 +3,15 @@ import { colors } from 'oolib'
 
 const { greyColor15, greyColor40, greyColor5 } = colors;
 
-// export const StyledTable = styled.table`
-//     margin-top: 2rem;
-//     border-collapse: collapse;
-// `
 export const StyledSurveyTableHead = styled.thead`
 
 `
-// export const StyledTableHead = styled.th`
-//     border: 1px solid ${greyColor15};
-//     padding: 2rem 1rem;
-//     text-align: start;
-//     background: white;
-//     position: sticky;
-//     top: 0;
-// `
-// export const StyledTableData = styled.td`
-//     border: 1px solid ${greyColor15};
-//     padding: 2rem;
-//     text-align: center;
-// `
 
 export const StyledTable = styled.table`
-    margin-top: 2rem;
     border-collapse: collapse;
+    width: inherit;
+    overflow-x: scroll;
+    border: 1px solid ${greyColor40};
 `
 
 export const StyledTableHead = styled.th`
@@ -40,7 +25,8 @@ export const StyledTableHead = styled.th`
 
 export const StyledTableData = styled.td`
     border: 1px solid ${greyColor40};
-    padding: ${({numOfContainers}) => numOfContainers > 2 ? '2rem' : '3rem'};
+    padding: 2rem;
+    /* padding: ${({numOfContainers}) => numOfContainers > 2 ? '2rem' : '3rem'}; */
     max-width: 32rem;
     /* height: 15rem; */
 `
